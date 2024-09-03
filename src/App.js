d144ea185a8acc9df8b29497a003079e60bd5baf
aishwaryap14 <aishwarya.potdar123@gamil.com>
2024-09-03 16:31:39 +0530

    Added: avg rating filter button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM  from 'react-dom/client'
-import { Restaurants } from './data'
+import Header from './Component/Header'
+import Body from './Component/Body'
 /*
 * Header
   - LOGO
@@ -20,58 +21,6 @@ import { Restaurants } from './data'
   - Address
   - Contact
 */
-
-const stylecard = {
-  backgroundColor : "#f0f0f0"
-}
-
-const Header = () => {
-  return(
-    <div className='header'>
-      <div className='logo-container'>
-        <img className='logo' src='https://res.cloudinary.com/dmabeivkl/image/upload/w_800,fl_lossy,f_auto/v1599564390/main-image/uyhji8rlbiqjmphel3k2.png'/>
-      </div>
-      <div className='nav-items'>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Cart</li>
-        </ul>
-      </div>
-    </div>
-  )
-}
-
-const RestaurantCard = (props) => {
-  console.log(Restaurants)
-  const {resData} = props
-  const {name, cuisines, avgRating, costForTwo} = resData?.info;
-  const {deliveryTime} = resData.info.sla;
-  return(
-    <div className='res-card' style={stylecard}>
-      <img className='res-logo' alt='res-logo' src = {"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + resData.info.cloudinaryImageId}/>
-      <h3>{name}</h3>
-      <h5>{cuisines.join(", ")}</h5>
-      <h5>{avgRating}</h5>
-      <h5>{costForTwo}</h5>
-      <h5>{deliveryTime} minutes</h5>
-    </div>
-  )
-}
-
-const Body = () => {
-  return(
-    <div className='body'>
-      <div className='search'>Search</div>
-      <div className='res-container'>
-        {/* <RestaurantCard resData = {Restaurants[0]}/> */}
-        {
-          Restaurants.map(restautant => <RestaurantCard key = {restautant.info.id} resData = {restautant}/>)
-        }
-      </div>
-    </div>
-  )
-}
 
 const AppLayout = () => {
   return(
